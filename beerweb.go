a16b4b102c51e44aea361efa144540bd22310ab6
Ian Molee <imolee@gmail.com>
2018-06-01 12:20:24 -0700

    Move concurrent fetching logic into beerweb package

--- beerweb.go
+++ beerweb.go
@@ -2,7 +2,10 @@
 // beer lists.
 package beerweb
 
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 // Taplister is an interface to be called and have returned a list of beers,
 // allowing for multiple strategies like HTML table scraping and API access.
@@ -10,6 +13,12 @@ type Taplister interface {
 	FetchBeers() ([]Beer, error)
 	Venue() string
 	URL() string // TODO This is slightly awkward.
+}
+
+type Taplist struct {
+	Venue string `json:"venue"`
+	URL   string `json:"url"`
+	Beers []Beer `json:"beers"`
 }
 
 // Beer describes a beer by brewery, name, and any other available attributes.
@@ -51,4 +60,68 @@ func (b Beer) String() string {
 // Extra details are gravy.
 func (b Beer) Valid() bool {
 	return b.Brewery != "" && b.Name != ""
+}
+
+// FetchAll is a helper to concurrently get all the beers for
+// a slice of Taplisters.
+//
+// TODO This is kind of ugly, but it allows us to use a common function
+// for fetching all venue's beers for all clients (e.g., CLI, web).
+func FetchAll(venues []Taplister) ([]Taplist, error) {
+	respCh := make(chan response)
+	taplists := make([]Taplist, 0, len(venues))
+
+	var err FetchAllError
+
+	defer func() { close(respCh) }()
+
+	for _, tl := range venues {
+		go fetch(tl, respCh)
+	}
+
+	for i := len(venues); i > 0; i-- {
+		tl := <-respCh
+		if tl.err != nil {
+			err = append(err, tl.err)
+			continue
+		}
+		taplists = append(taplists, Taplist{
+			Venue: tl.venue,
+			URL:   tl.url,
+			Beers: tl.beers,
+		})
+	}
+	return taplists, err
+}
+
+type FetchAllError []error
+
+func (e FetchAllError) Error() string {
+	b := strings.Builder{}
+	for _, err := range e {
+		b.WriteString(err.Error())
+		b.WriteByte('\n')
+	}
+	return b.String()
+}
+
+// TODO This can be simplified
+type response struct {
+	venue string
+	url   string
+	beers []Beer
+	err   error
+}
+
+func fetch(tl Taplister, ch chan<- response) {
+	beers, err := tl.FetchBeers()
+	if err != nil {
+		ch <- response{err: fmt.Errorf("error fetching beers from %s: %v\n", tl.Venue(), err)}
+		return
+	}
+	ch <- response{
+		venue: tl.Venue(),
+		url:   tl.URL(),
+		beers: beers,
+	}
 }
--- cmd/beerlist/beerlist.go
+++ cmd/beerlist/beerlist.go
@@ -11,64 +11,31 @@ import (
 	"github.com/ianfoo/beerweb/venues"
 )
 
-type response struct {
-	venue    string
-	url      string
-	beerList []beerweb.Beer
-	err      error
-}
-
 func main() {
 	jsonOutput := flag.Bool("json", false, "write output as JSON")
 	flag.Parse()
 
-	respCh := make(chan response)
-	defer func() { close(respCh) }()
-
-	for _, tl := range venues.Venues {
-		go fetch(tl, respCh)
+	taplists, err := beerweb.FetchAll(venues.Venues)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
 	}
 
-	for i := 0; i < len(venues.Venues); i++ {
-		resp := <-respCh
-		if resp.err != nil {
-			fmt.Println(resp.err)
-			continue
-		}
+	if *jsonOutput {
+		enc := json.NewEncoder(os.Stdout)
+		enc.Encode(taplists)
+		return
+	}
 
-		if *jsonOutput {
-			output := map[string]interface{}{
-				"Venue": resp.venue,
-				"URL":   resp.url,
-				"Beers": resp.beerList,
-			}
-			enc := json.NewEncoder(os.Stdout)
-			enc.Encode(output)
-			continue
-		}
-
-		banner := "Beer list for " + resp.venue
+	for i, taplist := range taplists {
+		banner := "Beer list for " + taplist.Venue
 		underline := strings.Repeat("=", len(banner))
 		fmt.Printf("%s\n%s\n", banner, underline)
-		for _, beer := range resp.beerList {
+		for _, beer := range taplist.Beers {
 			fmt.Println(beer)
 		}
-		if i < len(venues.Venues)-1 {
+		if i < len(taplists)-1 {
 			fmt.Println()
 		}
 	}
 
-}
-
-func fetch(tl beerweb.Taplister, ch chan<- response) {
-	beers, err := tl.FetchBeers()
-	if err != nil {
-		ch <- response{err: fmt.Errorf("error fetching beers from %s: %v\n", tl.Venue, err)}
-		return
-	}
-	ch <- response{
-		venue:    tl.Venue(),
-		url:      tl.URL(),
-		beerList: beers,
-	}
 }
